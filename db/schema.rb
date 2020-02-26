474eee134b24ca5b0e24d095b4b0e2c47b562fe3
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-02-27 01:01:52 +0900

    ログイン機能とアソシエーション実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_092352) do
+ActiveRecord::Schema.define(version: 2020_02_26_151421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_092352) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "content"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_02_26_092352) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
