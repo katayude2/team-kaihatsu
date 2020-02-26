98572a87ae3095e9f3c89c89f6443915aea0c4ca
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-02-26 23:54:23 +0900

    ビュー調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_091826) do
+ActiveRecord::Schema.define(version: 2020_02_26_092352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
